599c4ac821b0a471a3181045f4e9e6a1ea17c99a
Stefan Moises <moises@shoptimax.de>
2018-11-14 09:29:49 +0100

    fixes #3 - use OXID config if available

---
+++ config/database.php
@@ -0,0 +1,61 @@
+<?php
+// check if OXID shop available ...
+if (file_exists(__DIR__ . '/../../config.inc.php')) {
+    /**
+     * Helper for loading and getting the config file contents
+     */
+    class BootstrapConfigFileReader
+    {
+        /**
+         * BootstrapConfigFileReader constructor.
+         */
+        public function __construct()
+        {
+            include __DIR__ . '/../../config.inc.php';
+        }
+    }
+    // yes, so load OXID config file
+    $configFile = new \BootstrapConfigFileReader();
+    // and return specific config values
+    return [
+        'default' => env('DB_CONNECTION', 'mysql'),
+        'connections' => [
+            'mysql' => [
+                'driver' => 'mysql',
+                'host' => $configFile->dbHost,
+                'port' => $configFile->dbPort,
+                'database' => $configFile->dbName,
+                'username' => $configFile->dbUser,
+                'password' => $configFile->dbPwd,
+                'unix_socket' => env('DB_SOCKET', ''),
+                'charset' => env('DB_CHARSET', 'utf8mb4'),
+                'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+                'prefix' => env('DB_PREFIX', ''),
+                'strict' => env('DB_STRICT_MODE', true),
+                'engine' => env('DB_ENGINE', null),
+                'timezone' => env('DB_TIMEZONE', '+00:00'),
+            ],
+        ],
+    ];
+}
+// no OXID, use default .env vars
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 3306),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => env('DB_PREFIX', ''),
+            'strict' => env('DB_STRICT_MODE', true),
+            'engine' => env('DB_ENGINE', null),
+            'timezone' => env('DB_TIMEZONE', '+00:00'),
+        ],
+    ],
+];
